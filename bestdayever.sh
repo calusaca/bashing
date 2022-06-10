7227e6df20745431d3eefda0be946c5e1258d663
Carlos S <carlos@aldrin.com>
2022-06-09 22:22:19 -0600

    da

--- bestdayever.sh
+++ bestdayever.sh
@@ -7,11 +7,12 @@
 #name="Evil Ben"
 
 name=$1
+compliment=$2
 
 echo "Good morning $name!!"
 sleep 1
 echo "You're looking good today $name!!!"
 sleep 1
-echo "You have the best bear I've ever seen $name!!!"
+echo "You have the best $compliment I've ever seen $name!!!"
 
 
