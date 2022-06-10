93b0418c5885ba00f5f9d0e7963d17d494909b4d
Carlos S <carlos@aldrin.com>
2022-06-09 22:15:29 -0600

    Bashing from the start

---
+++ bestdayever.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "What is your name?"
+
+read name
+
+#name="Evil Ben"
+
+echo "Good morning $name!!"
+sleep 1
+echo "You're looking good today $name!!!"
+sleep 1
+echo "You have the best bear I've ever seen $name!!!"
+
+
